f5ad841efdfe905e25424245823059077c4c63bc
weizhengliang <lwz12@outlook.com>
2022-11-21 00:19:20 +0800

    half done

---
+++ Assets/Script/ItemCollector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ItemCollector : MonoBehaviour
+{
+
+    private int bananas = 0;
+
+    [SerializeField] private TextMeshProUGUI BananaText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Banana"))
+        {
+            Destroy(collision.gameObject);
+            bananas++;
+            BananaText.text = "Bananas: " + bananas;
+        }
+    }
+}
